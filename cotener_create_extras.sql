b60bca4982ee09992bea0f72f692d6a109e8655d
VictorAlonsoVergara <48967675+VictorAlonsoVergara@users.noreply.github.com>
2020-03-13 12:45:59 -0500

    Add files via upload

---
+++ cotener_create_extras.sql
@@ -0,0 +1,303 @@
+create or replace function buscar_hijos_arbol_fisico (id INTEGER)
+	RETURNS TABLE (
+		id_hijo INTEGER,
+		id_padre INTEGER
+)
+AS $$
+DECLARE
+	array_len INTEGER;
+	padre_rec record;
+	index INTEGER := 1;
+	array_padres INTEGER[];
+	array_hijos INTEGER[];
+BEGIN
+	FOR padre_rec IN (select * from "ARB_FISICO" where fis_id_padre = id) LOOP
+		SELECT array_append(array_padres,padre_rec.fis_id_padre) INTO array_padres;
+		SELECT array_append(array_hijos,padre_rec.fis_id) INTO array_hijos;
+	END LOOP;
+	IF array_length(array_padres,1) > 0 THEN
+		array_len := array_upper(array_padres,1);
+		WHILE index <= array_len LOOP
+			id_padre := array_padres[index];
+			id_hijo := array_hijos[index];
+			FOR padre_rec IN (select * from "ARB_FISICO" where fis_id_padre = array_hijos[index]) LOOP
+				SELECT array_append(array_padres,padre_rec.fis_id_padre) INTO array_padres;
+				SELECT array_append(array_hijos,padre_rec.fis_id) INTO array_hijos;
+				array_len := array_upper(array_padres,1);
+			END LOOP;
+			index := index + 1;
+			RETURN NEXT;
+		END LOOP;
+	END IF;
+END $$
+LANGUAGE plpgsql;
+
+create or replace function buscar_padres_arbol_fisico (id INTEGER)
+	RETURNS TABLE (
+		id_nodo INTEGER,
+		id_padre INTEGER
+)
+AS $$
+DECLARE
+	salir INTEGER := 1;
+	padre INTEGER;
+	hijo INTEGER;
+BEGIN
+	salir := 1;
+	hijo := id;
+	WHILE salir > 0 LOOP
+		padre := (select fis_id_padre from "ARB_FISICO" where fis_id = hijo limit 1);
+		id_nodo := hijo;
+		id_padre := padre;
+		IF id_padre IS NULL THEN
+			salir := 0;
+		END IF;
+		hijo := padre;
+		RETURN NEXT;
+	END LOOP;
+END $$
+LANGUAGE plpgsql;
+
+create or replace function buscar_hijos_arbol_logico (id INTEGER)
+	RETURNS TABLE (
+		id_hijo INTEGER,
+		id_padre INTEGER
+)
+AS $$
+DECLARE
+	array_len INTEGER;
+	padre_rec record;
+	index INTEGER := 1;
+	array_padres INTEGER[];
+	array_hijos INTEGER[];
+BEGIN
+	FOR padre_rec IN (select * from "ARB_LOGICO" where log_id_padre = id) LOOP
+		SELECT array_append(array_padres,padre_rec.log_id_padre) INTO array_padres;
+		SELECT array_append(array_hijos,padre_rec.log_id) INTO array_hijos;
+	END LOOP;
+	IF array_length(array_padres,1) > 0 THEN
+		array_len := array_upper(array_padres,1);
+		WHILE index <= array_len LOOP
+			id_padre := array_padres[index];
+			id_hijo := array_hijos[index];
+			FOR padre_rec IN (select * from "ARB_LOGICO" where log_id_padre = array_hijos[index]) LOOP
+				SELECT array_append(array_padres,padre_rec.log_id_padre) INTO array_padres;
+				SELECT array_append(array_hijos,padre_rec.log_id) INTO array_hijos;
+				array_len := array_upper(array_padres,1);
+			END LOOP;
+			index := index + 1;
+			RETURN NEXT;
+		END LOOP;
+	END IF;
+END $$
+LANGUAGE plpgsql;
+
+create or replace function buscar_padres_arbol_logico (id INTEGER)
+	RETURNS TABLE (
+		id_nodo INTEGER,
+		id_padre INTEGER
+)
+AS $$
+DECLARE
+	salir INTEGER := 1;
+	padre INTEGER;
+	hijo INTEGER;
+BEGIN
+	salir := 1;
+	hijo := id;
+	WHILE salir > 0 LOOP
+		padre := (select log_id_padre from "ARB_LOGICO" where log_id = hijo limit 1);
+		id_nodo := hijo;
+		id_padre := padre;
+		IF id_padre IS NULL THEN
+			salir := 0;
+		END IF;
+		hijo := padre;
+		RETURN NEXT;
+	END LOOP;
+END $$
+LANGUAGE plpgsql;
+
+create or replace function buscar_hijos_tipo_objeto (id INTEGER)
+	RETURNS TABLE (
+		id_hijo INTEGER,
+		id_padre INTEGER
+)
+AS $$
+DECLARE
+	array_len INTEGER;
+	padre_rec record;
+	index INTEGER := 1;
+	array_padres INTEGER[];
+	array_hijos INTEGER[];
+BEGIN
+	FOR padre_rec IN (select * from "MAT_TIPO_OBJ" where tobj_padre_id = id and tobj_estado = 'A') LOOP
+		SELECT array_append(array_padres,padre_rec.tobj_padre_id) INTO array_padres;
+		SELECT array_append(array_hijos,padre_rec.tobj_id) INTO array_hijos;
+	END LOOP;
+	IF array_length(array_padres,1) > 0 THEN
+		array_len := array_upper(array_padres,1);
+		WHILE index <= array_len LOOP
+			id_padre := array_padres[index];
+			id_hijo := array_hijos[index];
+			FOR padre_rec IN (select * from "MAT_TIPO_OBJ" where tobj_padre_id = array_hijos[index] and tobj_estado = 'A') LOOP
+				SELECT array_append(array_padres,padre_rec.tobj_padre_id) INTO array_padres;
+				SELECT array_append(array_hijos,padre_rec.tobj_id) INTO array_hijos;
+				array_len := array_upper(array_padres,1);
+			END LOOP;
+			index := index + 1;
+			RETURN NEXT;
+		END LOOP;
+	END IF;
+END $$
+LANGUAGE plpgsql;
+
+create or replace function buscar_padres_tipo_objeto (id INTEGER)
+	RETURNS TABLE (
+		id_nodo INTEGER,
+		id_padre INTEGER
+)
+AS $$
+DECLARE
+	salir INTEGER := 1;
+	padre INTEGER;
+	hijo INTEGER;
+BEGIN
+	salir := 1;
+	hijo := id;
+	WHILE salir > 0 LOOP
+		padre := (select tobj_padre_id from "MAT_TIPO_OBJ" where tobj_id = hijo limit 1);
+		id_nodo := hijo;
+		id_padre := padre;
+		IF id_padre IS NULL THEN
+			salir := 0;
+		END IF;
+		hijo := padre;
+		RETURN NEXT;
+	END LOOP;
+END $$
+LANGUAGE plpgsql;
+
+create or replace procedure borrar_rama_arbol_fisico (id IN INTEGER , borrados INOUT INTEGER[])
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	array_len INTEGER;
+	padre_rec record;
+	index INTEGER := 1;
+	array_padres INTEGER[];
+	array_hijos INTEGER[];
+BEGIN
+	FOR padre_rec IN (select * from "ARB_FISICO" where fis_id_padre = id) LOOP
+		SELECT array_append(array_padres,padre_rec.fis_id_padre) INTO array_padres;
+		SELECT array_append(array_hijos,padre_rec.fis_id) INTO array_hijos;
+	END LOOP;
+	IF array_length(array_padres,1) > 0 THEN
+		array_len := array_upper(array_padres,1);
+		WHILE index <= array_len LOOP
+			FOR padre_rec IN (select * from "ARB_FISICO" where fis_id_padre = array_hijos[index]) LOOP
+				SELECT array_append(array_padres,padre_rec.fis_id_padre) INTO array_padres;
+				SELECT array_append(array_hijos,padre_rec.fis_id) INTO array_hijos;
+				array_len := array_upper(array_padres,1);
+			END LOOP;
+			index := index + 1;
+		END LOOP;
+
+		SELECT array_prepend(id,array_hijos) INTO array_hijos;
+		borrados := array_hijos;
+		index := array_len+1;
+		WHILE index > 0 LOOP
+			DELETE FROM "ARB_FISICO" WHERE fis_id = array_hijos[index];
+			index := index - 1;
+		END LOOP;
+	ELSE
+		borrados := ARRAY [id];
+		DELETE FROM "ARB_FISICO" WHERE fis_id = id;
+	END IF;
+END $$;
+
+create or replace procedure borrar_rama_arbol_logico (id IN INTEGER , borrados INOUT INTEGER[])
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	array_len INTEGER;
+	padre_rec record;
+	index INTEGER := 1;
+	array_padres INTEGER[];
+	array_hijos INTEGER[];
+BEGIN
+	FOR padre_rec IN (select * from "ARB_LOGICO" where log_id_padre = id) LOOP
+		SELECT array_append(array_padres,padre_rec.log_id_padre) INTO array_padres;
+		SELECT array_append(array_hijos,padre_rec.log_id) INTO array_hijos;
+	END LOOP;
+	IF array_length(array_padres,1) > 0 THEN
+		array_len := array_upper(array_padres,1);
+		WHILE index <= array_len LOOP
+			FOR padre_rec IN (select * from "ARB_LOGICO" where log_id_padre = array_hijos[index]) LOOP
+				SELECT array_append(array_padres,padre_rec.log_id_padre) INTO array_padres;
+				SELECT array_append(array_hijos,padre_rec.log_id) INTO array_hijos;
+				array_len := array_upper(array_padres,1);
+			END LOOP;
+			index := index + 1;
+		END LOOP;
+
+		SELECT array_prepend(id,array_hijos) INTO array_hijos;
+		borrados := array_hijos;
+		index := array_len+1;
+		WHILE index > 0 LOOP
+			DELETE FROM "ARB_LOGICO" WHERE log_id = array_hijos[index];
+			index := index - 1;
+		END LOOP;
+	ELSE
+		borrados := ARRAY [id];
+		DELETE FROM "ARB_LOGICO" WHERE log_id = id;
+	END IF;
+END $$;
+
+create or replace procedure borrar_rama_tipo_objeto (id IN INTEGER , borrados INOUT INTEGER[])
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	array_len INTEGER;
+	padre_rec record;
+	index INTEGER := 1;
+	array_padres INTEGER[];
+	array_hijos INTEGER[];
+BEGIN
+	FOR padre_rec IN (select * from "MAT_TIPO_OBJ" where tobj_padre_id = id) LOOP
+		SELECT array_append(array_padres,padre_rec.tobj_padre_id) INTO array_padres;
+		SELECT array_append(array_hijos,padre_rec.tobj_id) INTO array_hijos;
+	END LOOP;
+	IF array_length(array_padres,1) > 0 THEN
+		array_len := array_upper(array_padres,1);
+		WHILE index <= array_len LOOP
+			FOR padre_rec IN (select * from "MAT_TIPO_OBJ" where tobj_padre_id = array_hijos[index]) LOOP
+				SELECT array_append(array_padres,padre_rec.tobj_padre_id) INTO array_padres;
+				SELECT array_append(array_hijos,padre_rec.tobj_id) INTO array_hijos;
+				array_len := array_upper(array_padres,1);
+			END LOOP;
+			index := index + 1;
+		END LOOP;
+
+		SELECT array_prepend(id,array_hijos) INTO array_hijos;
+		borrados := array_hijos;
+		index := array_len+1;
+		WHILE index > 0 LOOP
+			DELETE FROM "MAT_TIPO_OBJ" WHERE tobj_id = array_hijos[index];
+			index := index - 1;
+		END LOOP;
+	ELSE
+		borrados := ARRAY [id];
+		DELETE FROM "MAT_TIPO_OBJ" WHERE tobj_id = id;
+	END IF;
+END $$;
+
+create or replace view view_crons_consultas_objetos as
+select cron.cron_id,cons.con_trama_pregunta,
+		prot.prot_abreviacion,obj.obj_id,obj.obj_ip_address,obj.obj_usuario,obj.obj_password
+from "MAE_CRON" as cron 
+join "MAE_OBJETOS" as obj on obj.tobj_id = cron.tobj_id and obj.prot_id = cron.prot_id
+join "MAE_CONSULTAS" as cons on cons.prot_id = cron.prot_id and cons.mod_id = obj.mod_id
+join "TAB_PROTOCOLO" as prot on prot.prot_id = cron.prot_id
+where cron.cron_estado = 'A' and obj.obj_estado = 'A'
+order by cron.cron_id,cons.con_id,obj.obj_id;
---
+++ cotener_data_init.py
@@ -0,0 +1,126 @@
+import psycopg2
+from hashlib import blake2b
+
+con = psycopg2.connect(database='cotener',user='cotener',password='cotener')
+cur = con.cursor()
+
+tup = [(1,"Español","es","A"), (2,"English","en","A")]
+sql_stuff = 'INSERT INTO "MAE_IDIOMAS" (idi_id,idi_desc,idi_key,idi_estado) VALUES (%s,%s,%s,%s)'
+cur.executemany(sql_stuff,tup)
+con.commit()
+
+tup = [	(1, "Administrador","A"), (2,"Usuario","A")]
+sql_stuff = 'INSERT INTO "MAE_TIPO_USU" (tusu_id,tusu_desc,tusu_estado) VALUES (%s,%s,%s)'
+cur.executemany(sql_stuff,tup)
+con.commit()
+
+tup = [	(1, "mantenimiento_soft","Mantenimiento de software","A"),]
+sql_stuff = 'INSERT INTO "MAE_AREA" VALUES (%s,%s,%s,%s)'
+cur.executemany(sql_stuff,tup)
+con.commit()
+
+hToken = blake2b(digest_size=32)
+clave = "password"
+hToken.update(clave.encode())
+password = hToken.hexdigest()
+tup = (1, "Usuario Administrador",1,"A","correoAdministrador@mail.com","usu_administrador",password,1,1,1)
+sql_stuff = 'INSERT INTO "MAE_USUARIOS" VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+cur.execute(sql_stuff,tup)
+con.commit()
+
+tup = [	("estado_usuario", "Estados de usuario del sistema"),("estado_documento", "Estados de documento"),("estado_password_mayus", "Estado del flag de mayuscula para el password"),("estado_password_num", "Estado del flag de numero para el password"),("estado_password_carac", "Estado del flag de caracteres especiales para el password"),("longitud_pass", "longitud minima del password") ]
+sql_stuff = 'INSERT INTO "PARAM_GRUPO" (grupo_codigo,grupo_nombre) VALUES (%s,%s)'
+cur.executemany(sql_stuff,tup)
+con.commit()
+
+tup = [	("estado_usuario", "A", "Activo"),("estado_usuario", "D", "Desactivado"),("estado_usuario", "E", "Eliminado"),("estado_password_mayus", "A+1", "EA Activo / D Desactivado + numero minimo"),("estado_password_num", "A+1", "A Activo / D Desactivado + numero minimo"),("estado_password_carac", "A+1", "A Activo / D Desactivado + numero minimo"),("longitud_pass", "12", "Longitud de la cadena"), ]
+sql_stuff = 'INSERT INTO "PARAM" (param_grupo,param_id,param_valor) VALUES (%s,%s,%s)'
+cur.executemany(sql_stuff,tup)
+con.commit()
+
+insert_menu = ('INSERT INTO "TAB_MENU" (menu_desc,menu_name,menu_ord) VALUES (%s,%s,%s)')
+data_menu = [('Permiso para tipo de usuario','Tipo Usuario',2),
+('Permiso para crons','Cron',14),
+('Permiso para Tipo de objetos','Tipo Objeto',11),
+('Permiso para ejecuciones','Ejecuciones',22),
+('Permiso para consultas','Consultas',17),
+('Permiso para reportes','Reportes',20),
+('Permiso para objetos','Objetos',12),
+('Permiso para accesos a reportes','Usuario Acceso Reportes',6),
+('Permiso para alertas de usuario','Usuario Alertas',5),
+('Permiso para acceso de arboles','Usuario Acceso ArboLes',7),
+('Permiso para arboles fisicos','Arboles Fisicos',18),
+('Permiso para arboles logicos','Arboles Logicos',19),
+('Permiso para objetos fisicos','Objetos Fisicos',23),
+('Permiso para objetos logicos','Objetos Logicos',24),
+('Permiso para marcas','Marcas',13),
+('Permiso para area','Areas',10),
+('Permiso para idiomas','Idiomas',8),
+('Permios para modelos','Modelos',15),
+('Permiso para menu de usuarios','Usuarios Menu',3),
+('Permiso para menus','Menu',9),
+('Permiso para protocolos','Protocolos',16),
+('Permiso para acciones','Acciones',21),
+('Permiso para acciones de usuarios','Acciones usuario', 4),
+('Permiso para usuarios','Usuarios',1)]
+
+cur.executemany(insert_menu,data_menu)
+con.commit()
+
+insert_reportes = ('INSERT INTO "TAB_REPORTES" (rep_desc,rep_estado,rep_ldesc) VALUES (%s,%s,%s)')
+data_reportes = ('Reporte Ejemplo','A','Reporte de ejemplo al inicar el servidor')
+cur.execute(insert_reportes,data_reportes)
+con.commit()
+
+insert_reportes = ('INSERT INTO "MAE_USU_ACCESOS_REPORTES" (rep_id,usu_id,urep_estado) VALUES (%s,%s,%s)')
+data_reportes = (1,1,True)
+cur.execute(insert_reportes,data_reportes)
+con.commit()
+
+insert_prot = ('INSERT INTO "TAB_PROTOCOLO" (prot_desc,prot_abreviacion) VALUES (%s,%s)')
+data_prot = [('SNMP','S'),('SSH','H'),('Telnet','T'),('TR01','0')]
+cur.executemany(insert_prot,data_prot)
+con.commit()
+
+tup = [	("Ver usuario",),("Listar usuario",),("Crear usuario",),("Modificar usuario",),("Borrar usuario",),("Autenticar usuario",),("Cerrar usuario",),("Desactivar usuario",),("Activar usuario",),("Eliminar usuario",),
+		("Ver tipo usuario",),("Listar tipo usuario",),("Crear tipo usuario",),("Modificar tipo usuario",),("Borrar tipo usuario",),
+		("Ver cron",),("Listar cron",),("Crear cron",),("Modificar cron",),("Borrar cron",),
+		("Ver tipo objeto",),("Listar tipo objeto",),("Crear tipo objeto",),("Modificar tipo objeto",),("Borrar tipo objeto",),("Borrar todo tipo objeto",),("Buscar hijos tipo objeto",),("Buscar padres tipo objeto",),("Buscar hermanos tipo objeto",),
+		("Ver ejecuciones",),("Listar ejecuciones",),
+		("Ver consultas",),("Listar consultas",),("Crear consultas",),("Modificar consultas",),("Borrar consultas",),
+		("Ver reportes",),("Listar reportes",),("Crear reportes",),("Modificar reportes",),("Borrar reportes",),
+		("Ver objeto",),("Listar objeto",),("Crear objeto",),("Modificar objeto",),("Borrar objeto",),
+		("Ver usuario acceso indicadores",),("Listar usuario acceso indicadores",),("Crear usuario acceso indicadores",),("Modificar usuario acceso indicadores",),("Borrar usuario acceso indicadores",),
+		("Ver usuario acceso reportes",),("Listar usuario acceso reportes",),("Crear usuario acceso reportes",),("Modificar usuario acceso reportes",),("Borrar usuario acceso reportes",),
+		("Ver usuario alertas",),("Listar usuario alertas",),("Crear usuario alertas",),("Modificar usuario alertas",),("Borrar usuario alertas",),
+		("Ver usuario accesos arboles",),("Listar usuario accesos arboles",),("Crear usuario accesos arboles",),("Modificar usuario accesos arboles",),("Borrar usuario accesos arboles",),
+		("Ver arboles fisicos",),("Listar arboles fisicos",),("Crear arboles fisicos",),("Modificar arboles fisicos",),("Borrar arboles fisicos",),("Borrar todo arboles fisicos",),("Buscar hijos arboles fisicos",),("Buscar padres arboles fisicos",),("Buscar hermanos arboles fisicos",),
+		("Ver arboles logicos",),("Listar arboles logicos",),("Crear arboles logicos",),("Modificar arboles logicos",),("Borrar arboles logicos",),("Borrar todo arboles logicos",),("Buscar hijos arboles logicos",),("Buscar padres arboles logicos",),("Buscar hermanos arboles logicos",),
+		("Ver objeto fisico",),("Listar objeto fisico",),("Crear objeto fisico",),("Modificar objeto fisico",),("Borrar objeto fisico",),
+		("Ver objeto logico",),("Listar objeto logico",),("Crear objeto logico",),("Modificar objeto logico",),("Borrar objeto logico",),
+		("Ver marcas",),("Listar marcas",),("Crear marcas",),("Modificar marcas",),("Borrar marcas",),
+		("Ver area",),("Listar area",),("Crear area",),("Modificar area",),("Borrar area",),
+		("Ver idiomas",),("Listar idiomas",),("Crear idiomas",),("Modificar idiomas",),("Borrar idiomas",),
+		("Ver modelo",),("Listar modelo",),("Crear modelo",),("Modificar modelo",),("Borrar modelo",),
+		("Ver usuario menu",),("Listar usuario menu",),("Crear usuario menu",),("Modificar usuario menu",),("Borrar usuario menu",),("Menus usuario menu",),("Editar usuario menu",),
+		("Ver tab menu",),("Listar tab menu",),
+		("Ver tab acciones",),("Listar tab acciones",),
+		("Ver tab protocolo",),("Listar tab protocolo",),
+		("Ver tab param",),("Ver tab param grupo",),
+		("Check token",)]
+
+sql_stuff = 'INSERT INTO "TAB_ACCIONES" (acc_desc,acc_id) VALUES (%s,%s)'
+conteo = 393
+for i in tup :
+	#print(i)
+	cur.execute(sql_stuff,(i[0],conteo))
+	con.commit()
+	conteo = conteo + 1
+con.close()
+
+insert_usuarios_menu = ('INSERT INTO "MAE_USUARIOS_MENU" (usu_id,menu_id,estado_umenu) VALUES (%s,%s,%s)')
+data_usuarios_menu = []
+for ind in range(len(data_menu)):
+	data_usuarios_menu.append((1,ind+1,True))
+cur.executemany(insert_usuarios_menu,data_usuarios_menu)
+con.commit()
